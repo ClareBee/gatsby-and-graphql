8fdc51d35255f2c5587f89d929999558d0f62faa
Clare <chblackburne@gmail.com>
2018-09-29 17:03:27 +0100

    Adds basic graphql query for title text

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Header from "../components/header";
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <Header headerText="About Gatsby" />
+    <Header headerText={data.site.siteMetadata.title} />
     <p>Such wow. Very React.</p>
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
